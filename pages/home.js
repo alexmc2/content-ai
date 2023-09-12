53e63dada05c70ba101e1a3f5a10b5d4f293c0fe
Alex <alex_mcgarry@hotmail.co.uk>
2023-09-12 02:39:45 +0100

    add sample image gallery

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { withPageAuthRequired, getSession } from '@auth0/nextjs-auth0';
+
 import { getAppProps } from '../utils/getAppProps';
 import clientPromise from '../lib/mongodb';
 import { useExtendedUser } from '../context/userContext';
@@ -13,6 +14,27 @@ import {
 } from '@material-tailwind/react';
 import { PhotoIcon, PencilSquareIcon } from '@heroicons/react/24/outline';
 
+async function populateUserGalleryWithSampleImages(userId) {
+  const client = await clientPromise;
+  const db = client.db('Content-AI');
+
+  // Fetch all sample images
+  const sampleImages = await db.collection('sampleImages').find({}).toArray();
+
+  // Prepare images for insertion: Add userId and remove isSample field
+  const imagesToInsert = sampleImages.map((image) => {
+    return {
+      imageUrl: image.imageUrl,
+      prompt: image.prompt,
+      userId: userId, // Use the passed userId directly
+      created: new Date(),
+    };
+  });
+
+  // Insert the images into the user's image collection
+  await db.collection('images').insertMany(imagesToInsert);
+}
+
 export default function HomePage() {
   const { user } = useExtendedUser();
   if (!user)
@@ -23,7 +45,7 @@ export default function HomePage() {
     );
 
   return (
-    <div className="flex justify-center items-center h-screen mx-2 my-5 ">
+    <div className="flex justify-center   mx-2 my-5 ">
       <Card className="bg-white/90 px-2 pb-10 flex  border-sky-100 mx-auto max-w-screen-md w-full prose shadow-sm ">
         <CardBody>
           <img
@@ -31,16 +53,36 @@ export default function HomePage() {
             alt="Robot Icon"
             className="mx-auto h-16 w-16"
           />
-          <Typography variant="h5" color="blue-gray" className="mb-2">
-            Hello, {user.name}!
-          </Typography>
+          <Typography variant="h5">Hello, {user.name}!</Typography>
           <Typography className="mb-0">
-            Welcome to Vertex AI. Your account has been credited with 20 tokens.
-            Each document or image generated deducts one token but you can
-            acquire more on the tokens page. <br></br>
-            <Typography variant="h5" color="blue-gray" className="mb-2"><br></br>
-            How it works
-          </Typography>
+            Welcome to Vertex AI - an app for AI-powered text and image
+            generation <br></br>
+            <Typography variant="h5">
+              <br></br>
+              How it works
+            </Typography>
+            <br></br>
+            Vertex AI harnesses the capabilities of OpenAI&apos;s GPT-3.5-turbo
+            API for written content and Stability AI&apos;s powerful new SDXL
+            1.0 model for image generation. Click the &apos;New Post&apos; or
+            &apos;New Image&apos; buttons to get started. <br></br>
+            <br></br>
+            You can view your generated content by clicking on the history
+            button on the sidebar. New posts are automatically saved to your
+            document history, and images can be saved by clicking the
+            &apos;Save&apos; button after the image has been generated. A sample
+            gallery of AI generated images can be found in your images history.
+            <br></br>
+            <br></br>
+            Your account has been credited with 20 tokens. Each document or
+            image generated deducts one token but you can acquire more on the
+            tokens page. <br></br>
+            <br></br>
+            Please be aware that the Open AI post generator is a bit on the slow
+            side and can take around 30 seconds to return a result. I&apos;m
+            currently exploring ways to boost this speed and stream the API
+            response (word-by-word) with edge functions. Stay tuned for updates!
+            <br></br>
             <br></br>This app was developed as a portfolio practice project and
             your thoughts, feedback, and bug reports are welcome! You can
             contact me at alex_mcgarry@hotmail.com or WhatsApp on 07793 565 433.
@@ -92,7 +134,8 @@ export const getServerSideProps = withPageAuthRequired({
     });
 
     if (!userProfile) {
-      await db.collection('users').insertOne({
+      // Insert the new user
+      const insertResult = await db.collection('users').insertOne({
         auth0Id: user.sub,
         email: user.email,
         username: user.username,
@@ -104,18 +147,11 @@ export const getServerSideProps = withPageAuthRequired({
         updated_at: user.updated_at,
         availableTokens: 20,
       });
-      userProfile = {
-        auth0Id: user.sub,
-        email: user.email,
-        username: user.username,
-        email_verified: user.email_verified,
-        name: user.name,
-        nickname: user.nickname,
-        picture: user.picture,
-        sid: user.sid,
-        updated_at: user.updated_at,
-        availableTokens: 20,
-      };
+
+      // Fetch the inserted user's _id
+      const insertedUserId = insertResult.insertedId;
+
+      await populateUserGalleryWithSampleImages(insertedUserId);
     }
 
     return {
