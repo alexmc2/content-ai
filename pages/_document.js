e3e15e955ed5bdc489906210e5382c4b28749bc5
Alex <alex_mcgarry@hotmail.co.uk>
2023-09-13 16:41:44 +0100

    image for tags

--- pages/_document.js
+++ pages/_document.js
@@ -6,16 +6,37 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* Open Graph Tags */}
-          <meta property="og:title" content="Vertex-AI: AI-powered content and image generator" />
-          <meta property="og:description" content="Web application that utilises Open AI’s GPT-3.5-turbo API and more." />
-          <meta property="og:image" content="https://res.cloudinary.com/drbz4rq7y/image/upload/v1694618769/Screenshot_from_2023-09-13_16-24-33_fjjliv.png" />
-          <meta property="og:url" content="https://content-ai-brown.vercel.app" />
+          <meta
+            property="og:title"
+            content="Vertex-AI: AI-powered content and image generator"
+          />
+          <meta
+            property="og:description"
+            content="Web application that utilises Open AI’s GPT-3.5-turbo API and more."
+          />
+          <meta
+            property="og:image"
+            content="https://asset.cloudinary.com/drbz4rq7y/27c0e3319a01b5ade2b01c0c5f8d4ea0"
+          />
+          <meta
+            property="og:url"
+            content="https://content-ai-brown.vercel.app"
+          />
 
           {/* Twitter Card Tags */}
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Vertex-AI: AI-powered content and image generator" />
-          <meta name="twitter:description" content="Web application that utilises Open AI’s GPT-3.5-turbo API and more." />
-          <meta name="twitter:image" content="https://res.cloudinary.com/drbz4rq7y/image/upload/v1694618769/Screenshot_from_2023-09-13_16-24-33_fjjliv.png" />
+          <meta
+            name="twitter:title"
+            content="Vertex-AI: AI-powered content and image generator"
+          />
+          <meta
+            name="twitter:description"
+            content="Web application that utilises Open AI’s GPT-3.5-turbo API and more."
+          />
+          <meta
+            name="twitter:image"
+            content="https://asset.cloudinary.com/drbz4rq7y/27c0e3319a01b5ade2b01c0c5f8d4ea0"
+          />
           {/* ... other head elements */}
         </Head>
         <body>
