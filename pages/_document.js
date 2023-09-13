a086979a4b00d8077d7c59fb3d5394861ecd689f
Alex <alex_mcgarry@hotmail.co.uk>
2023-09-13 17:16:52 +0100

    image

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ class MyDocument extends Document {
           />
           <meta
             property="og:image"
-            content="https://res.cloudinary.com/drbz4rq7y/image/upload/c_scale,h_1200,q_100,r_8,w_880/v1694618769/Screenshot_from_2023-09-13_16-24-33_fjjliv.png"
+            content="https://res.cloudinary.com/drbz4rq7y/image/upload/v1694375394/uswf7yaq5bmuwhoz7fhz.jpg"
           />
           <meta
             property="og:url"
@@ -35,7 +35,7 @@ class MyDocument extends Document {
           />
           <meta
             name="twitter:image"
-            content="https://res.cloudinary.com/drbz4rq7y/image/upload/c_scale,h_1200,q_100,r_8,w_880/v1694618769/Screenshot_from_2023-09-13_16-24-33_fjjliv.png"
+            content="https://res.cloudinary.com/drbz4rq7y/image/upload/v1694375394/uswf7yaq5bmuwhoz7fhz.jpg"
           />
           {/* ... other head elements */}
         </Head>
