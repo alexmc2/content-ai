f4daab42ac342d417d34965a5846409a605f52e1
Alex <alex_mcgarry@hotmail.co.uk>
2023-09-13 16:30:40 +0100

    graph tags

---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Open Graph Tags */}
+          <meta property="og:title" content="Vertex-AI: AI-powered content and image generator" />
+          <meta property="og:description" content="Web application that utilises Open AI’s GPT-3.5-turbo API and more." />
+          <meta property="og:image" content="https://res.cloudinary.com/drbz4rq7y/image/upload/v1694618769/Screenshot_from_2023-09-13_16-24-33_fjjliv.png" />
+          <meta property="og:url" content="https://content-ai-brown.vercel.app" />
+
+          {/* Twitter Card Tags */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Vertex-AI: AI-powered content and image generator" />
+          <meta name="twitter:description" content="Web application that utilises Open AI’s GPT-3.5-turbo API and more." />
+          <meta name="twitter:image" content="https://res.cloudinary.com/drbz4rq7y/image/upload/v1694618769/Screenshot_from_2023-09-13_16-24-33_fjjliv.png" />
+          {/* ... other head elements */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
